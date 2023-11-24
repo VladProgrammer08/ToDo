3eec06433141389c67c5d4d30639676e6a48e4e5
VladProgrammer08 <bumbuv136@gmail.com>
2023-11-23 23:01:42 -0800

    Added more functions that will let the user to create more tasks

--- js/main.js
+++ js/main.js
@@ -4,3 +4,22 @@ window.onload = function () {
     let addBtn = document.querySelector("#add_button");
     addBtn.onclick = addTask;
 };
+function addTask() {
+    let taskBox = document.querySelector("#task");
+    let task = taskBox.value;
+    let taskImportanceBox = document.querySelector("#importance");
+    let taskImportance = taskImportanceBox.value;
+    let newTasks = new Tasks();
+    newTasks.task = task;
+    newTasks.importance = taskImportance;
+    let taskDiv = document.createElement("div");
+    let taskHeading = document.createElement("h2");
+    let taskImportancePara = document.createElement("p");
+    taskHeading.textContent = newTasks.task;
+    taskImportancePara.textContent = newTasks.importance;
+    taskDiv.appendChild(taskHeading);
+    taskDiv.appendChild(taskImportancePara);
+    let displayDiv = document.querySelector("#display");
+    displayDiv.appendChild(taskDiv);
+    taskDiv.onclick = taskDivClicker;
+}
--- ts/main.ts
+++ ts/main.ts
@@ -7,3 +7,37 @@ window.onload = function(){
     let addBtn = document.querySelector("#add_button") as HTMLButtonElement;
     addBtn.onclick = addTask;
 }
+function addTask(){
+    
+
+    // Get the data from the form
+    let taskBox = document.querySelector("#task") as HTMLInputElement;
+    let task = taskBox.value;
+    let taskImportanceBox = document.querySelector("#importance") as HTMLInputElement;
+    let taskImportance = taskImportanceBox.value;
+
+    // Populate object
+    let newTasks = new Tasks();
+    newTasks.task = task;
+    newTasks.importance = taskImportance;
+    
+
+    // Display the new product
+
+   let taskDiv = document.createElement("div");
+   let taskHeading = document.createElement("h2");
+   let taskImportancePara = document.createElement("p");
+   taskHeading.textContent = newTasks.task;
+   taskImportancePara.textContent = newTasks.importance;
+
+   // Add elements to product div
+   taskDiv.appendChild(taskHeading);
+   taskDiv.appendChild(taskImportancePara);
+
+   // Add product to the page
+   let displayDiv = document.querySelector("#display") as HTMLDivElement;
+   displayDiv.appendChild(taskDiv);
+
+   // Add onclick to the prod div
+   taskDiv.onclick = taskDivClicker;
+}
